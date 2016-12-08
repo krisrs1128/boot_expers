d72842af016a3b57acc96b91e8a9a54976d6bbfa
Kris Sankaran <kriss1@sherlock-ln02.stanford.edu>
2016-12-08 11:42:18 -0800

    add overall batch fitting code

---
+++ src/batch.R
@@ -0,0 +1,46 @@
+#! /usr/bin/env Rscript
+
+# File description -------------------------------------------------------------
+
+
+output_path <- function(dir) {
+  function(filename) {
+    file.path(dir, filename)
+  }
+}
+
+create_job <- function(outfile,
+                       jobname,
+                       script_lines,
+                       modules = c("R/3.3.0"),
+                       time_alloc = "00:15:00",
+                       mem_alloc = 2000) {
+  slurm_opts <- c("#!/bin/bash",
+                  "#",
+                  "#all commands that start with SBATCH contain commands that are just used by SLURM for scheduling",
+                  "#################",
+                  "#set a job name",
+                  sprintf("#SBATCH --job-name=%s", jobname),
+                  "#################",
+                  "#a file for job output, you can check job progress",
+                  sprintf("#SBATCH --output=%s.out", jobname),
+                  "#################",
+                  "# a file for errors from the job",
+                  sprintf("#SBATCH --error=%s.err", jobname),
+                  "#################",
+                  "#time you think you need; default is one hour",
+                  "#in minutes in this case, hh:mm:ss",
+                  sprintf("#SBATCH --time=%s", time_alloc),
+                  "#################",
+                  "#memory per node; default is 4000 MB per CPU",
+                  sprintf("#SBATCH --mem=%d", mem_alloc)
+                  )
+  ml_opts <- paste("ml", modules) %>%
+    paste(collapse = "\n")
+
+  cat(
+    c(slurm_opts, ml_opts, script_lines),
+    sep = "\n",
+    file = outfile
+  )
+}
