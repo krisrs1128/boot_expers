d9e632a87b628c1422d12f52081ac8d02fe521fa
Kris Sankaran <sankaran.kris@gmail.com>
2016-12-09 11:48:44 -0800

    function for plotting reductions of the simplex

--- vis/vis_utils.R
+++ vis/vis_utils.R
@@ -4,9 +4,9 @@
 
 #' Read bootstrap replicates into a single data.frame
 #'
-#' @param files {character vector} A vector of paths pointing to the
+#' @param files [character vector] A vector of paths pointing to the
 #'   bootstrap replicates to read in and combine.
-#' @return data {data.frame} A data.frame with the combined data,
+#' @return data [data.frame] A data.frame with the combined data,
 #'   along with the file paths and bootstrap replicates associated
 #'   with each row.
 combine_replicates <- function(files) {
@@ -22,4 +22,24 @@ combine_replicates <- function(files) {
   }
 
   do.call(rbind, data)
+}
+
+#' Plot points on a face of a high dimensional simplex
+#'
+#' @param p [matrix] An n x V matrix, whose rows are point on a V
+#'   dimensional simplex.
+#' @param coords [vector] A length 3 vector specifying the dimensions
+#'   to project the points onto.
+#' @return ggtern [ggtern object] A ggtern plot object of the points
+#'   on the specified
+simplex_proj <- function(p, coords) {
+  mp <- p[, coords] %>%
+    melt() %>%
+    dcast(Var1 ~ Var2)
+  colnames(mp)[-1] <- paste0("dim_", colnames(mp)[-1])
+  dims <- colnames(mp)[-1]
+
+  ggtern(mp) +
+    geom_point(aes_string(x = dims[1], y = dims[2], z = dims[3])) +
+    theme_nogrid()
 }
