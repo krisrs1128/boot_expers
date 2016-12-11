9209cd468647f7d232c37c1cda400b94c47b60d8
Kris Sankaran <sankaran.kris@gmail.com>
2016-12-11 15:32:51 -0800

    run visualization experiments as Rscripts

--- vis/vis_exper.R
+++ vis/vis_exper.R
@@ -2,7 +2,11 @@
 
 # File description -------------------------------------------------------------
 
+# Rscript vis/vis_exper.R /scratch/users/kriss1/working/boot_expers/expers/exper_d20_n20.json
+
 ## ---- setup ----
+args <- commandArgs(trailingOnly=TRUE) # give paths for experiment json
+#args <- list("/scratch/users/kriss1/working/boot_expers/expers/exper_d20_n20.json")
 library("jsonlite")
 library("feather")
 library("plyr")
@@ -27,13 +31,13 @@ min_theme <- theme_update(
   legend.key = element_blank()
 )
 
-setwd("/scratch/users/kriss1/working/boot_expers")
 source("vis/vis_utils.R")
-exper <- fromJSON("expers/exper_d100_n100.json")
+exper <- fromJSON(args[[1]])
 
 ## ---- read-output ----
 paths <- exper$paths
 output_dir <- file.path(paths$base, paths$output_dir)
+plot_dir <- file.path(paths$base, paths$plot_dir)
 beta <- output_dir %>%
   list.files("beta_boot_[0-9]+", full.names = TRUE) %>%
   combine_replicates()
@@ -49,7 +53,7 @@ theta_truth$n <- seq_len(nrow(theta_truth))
 theta_truth <- theta_truth %>%
   melt(measure.vars = c("V1", "V2"), variable.name = "k")
 
-theta_master <- output_dir %>%
+theta_fit <- output_dir %>%
   list.files("theta_hat_vb", full.names = TRUE) %>%
   read_feather()
 
@@ -61,23 +65,16 @@ mbeta_truth <- beta_truth %>%
   melt(id.vars = "k", variable.name = "v")
 mbeta_truth$v <- gsub("V", "", mbeta_truth$v)
 
-beta_master <- output_dir %>%
+beta_fit <- output_dir %>%
   list.files("beta_hat_vb", full.names = TRUE) %>%
   read_feather()
-colnames(beta_master)[1] <- "k"
-beta_master <- beta_master %>%
+colnames(beta_fit)[1] <- "k"
+beta_fit <- beta_fit %>%
   melt(id.vars = "k", variable.name = "v")
 
 ## ---- vis-theta ----
-ggplot() +
-  geom_histogram(data = theta, aes(x = theta), binwidth = 0.01) +
-  geom_vline(data = theta_truth, aes(xintercept = value), linetype = 1, col = "#696969") +
-  geom_vline(data = theta_master, aes(xintercept = theta), linetype = 2, col = "#696969") +
-  facet_wrap(~n) +
-  theme(
-    panel.border = element_rect(fill = "transparent", size = 0.4),
-    panel.spacing = unit(0, "line")
-  )
+theta_plot(list("samples" = theta, "truth" = theta_truth, "fit" = theta_fit)) %>%
+  ggsave(file = file.path(plot_dir, "theta_unaligned.pdf"), device = NULL)
 
 ## ---- vis-beta ----
 mbeta <- beta %>%
@@ -95,19 +92,10 @@ v_order <- mbeta %>%
 
 mbeta$v <- factor(mbeta$v, levels = v_order)
 mbeta_truth$v <- factor(mbeta_truth$v, levels = v_order)
-beta_master$v <- factor(beta_master$v, levels = v_order)
+beta_fit$v <- factor(beta_fit$v, levels = v_order)
 
-ggplot() +
-  geom_hline(yintercept = 0, size = 0.1, col = "#696969") +
-  geom_histogram(data = mbeta, aes(x = value), binwidth = 0.003) +
-  geom_vline(data = mbeta_truth, aes(xintercept = value, y = 0), col = "#696969", size = 0.5, linetype = 1) +
-  geom_vline(data = beta_master, aes(xintercept = value, y = 0), col = "#696969", size = 0.5, linetype = 2) +
-  scale_y_continuous(expand = c(0, 0)) +
-  coord_flip() +
-  facet_grid(. ~ v) +
-  theme(
-    panel.spacing = unit(0, "line")
-  )
+beta_plot(list("samples" = mbeta, "truth" = mbeta_truth, "fit" = beta_fit)) %>%
+  ggsave(file = file.path(plot_dir, "beta_unaligned.pdf"), device = NULL)
 
 ## ---- tours ----
 projs <- combn(exper$model$V, 3)
@@ -120,11 +108,11 @@ p_beta <- mbeta %>%
 
 for (i in seq_len(10)) {
   simplex_proj(p_beta, projs[, i]) %>%
-    print()
+    ggsave(file = file.path(plot_dir, paste0("simplex_proj_", i, ".pdf")), device = NULL)
 }
 
 ## ---- correspondence-analysis ----
-p_beta_master <- beta_master %>%
+p_beta_fit <- beta_fit %>%
   dcast(k ~ v) %>%
   select(-k) %>%
   as.matrix()
@@ -136,7 +124,7 @@ p_beta_truth <- mbeta_truth %>%
 
 p_beta <- rbind(
   data.frame(type = "bootstrap", p_beta),
-  data.frame(type = "master", p_beta_master),
+  data.frame(type = "fit", p_beta_fit),
   data.frame(type = "truth", p_beta_truth)
 )
 
@@ -146,107 +134,75 @@ beta_row_coords <- data.frame(
   ca_beta$row$coord
 )
 
-ggplot() +
+p <- ggplot() +
   geom_point(data = beta_row_coords %>% filter(type == "bootstrap"),
              aes(x = Dim.1, y = Dim.2, col = type), size = .5, alpha = 0.5) +
   geom_point(data = beta_row_coords %>% filter(type != "bootstrap"),
              aes(x = Dim.1, y = Dim.2, col = type)) +
   scale_color_brewer(palette = "Set2") +
   coord_fixed()
+ggsave(p, file = file.path(plot_dir, "correspondence_analysis.pdf"))
 
 ## ---- alignment-approach ----
 R <- max(mbeta$rep)
-beta_master_mat <- beta_master %>%
+beta_fit_mat <- beta_fit %>%
   dcast(k ~ v) %>%
   select(-k) %>%
   as.matrix()
 
 old_names <- colnames(mbeta)
 colnames(mbeta) <- c("file", "rep", "row", "col", "value")
-mbeta <- match_matrices(mbeta, beta_master_mat)
+mbeta <- match_matrices(mbeta, beta_fit_mat)
 colnames(mbeta) <- old_names
-
 mbeta_truth$k <- rep(c(2, 1), 10)
 
 ## ---- visualize-aligned ----
-ggplot() +
-  geom_vline(data = mbeta_truth, aes(xintercept = value, y = 0, col = as.factor(k)), size = 0.5, linetype = 1) +
-  geom_vline(data = beta_master, aes(xintercept = value, y = 0, col = as.factor(k)), size = 0.5, linetype = 2) +
-  geom_histogram(data = mbeta,
-                 aes(x = value, fill = as.factor(k)),
-                 binwidth = .003, alpha = 0.8, position = "identity") +
-  geom_hline(yintercept = 0, size = 0.1, col = "#696969") +
-  scale_fill_brewer(palette = "Set2") +
-  scale_color_brewer(palette = "Set2") +
-  scale_y_continuous(expand = c(0, 0)) +
-  coord_flip() +
-  facet_grid(. ~ v) +
-  theme(
-    panel.spacing = unit(0, "line")
-  )
+beta_plot(
+  list("samples" = mbeta, "truth" = mbeta_truth, "fit" = beta_fit),
+  aligned = TRUE
+) %>%
+  ggsave(file = file.path(plot_dir, "beta_aligned.pdf"))
 
 ## ---- align-theta ----
-theta_master_mat <- theta_master %>%
+theta_fit_mat <- theta_fit %>%
   dcast(k ~ n) %>%
   select(-k) %>%
   as.matrix()
 
 old_names <- colnames(theta)
 colnames(theta) <- c("file", "rep", "col", "row", "value")
-theta <- match_matrices(theta, theta_master_mat)
+theta <- match_matrices(theta, theta_fit_mat)
 colnames(theta) <- old_names
 
-ggplot() +
-  geom_histogram(data = theta, aes(x = theta, fill = as.factor(k)),
-                 binwidth = 0.01, alpha = 0.8, position = "identity") +
-  geom_vline(data = theta_truth, aes(xintercept = value), linetype = 1, col = "#696969") +
-  geom_vline(data = theta_master, aes(xintercept = theta), linetype = 2, col = "#696969") +
-  scale_fill_brewer(palette = "Set2") +
-  scale_y_continuous(limits = c(0, 150) , oob = rescale_none) +
-  facet_wrap(~n) +
-  theme(
-    panel.border = element_rect(fill = "transparent", size = 0.4),
-    panel.spacing = unit(0, "line")
-  )
+theta_plot(
+  list("samples" = theta, "truth" = theta_truth, "fit" = theta_fit),
+  aligned = TRUE
+) %>%
+  ggsave(file = file.path(plot_dir, "theta_aligned.pdf"))
 
 ## ---- vb-samples-theta ----
 theta_samples <- file.path(output_dir, "theta_samples_vb.feather") %>%
   read_feather()
 colnames(theta_samples) <- c("rep", "n", "k", "theta")
 
-ggplot() +
-  geom_histogram(data = theta_samples, aes(x = theta, fill = as.factor(k)),
-                 binwidth = 0.01, alpha = 0.8, position = "identity") +
-  geom_vline(data = theta_truth, aes(xintercept = value), linetype = 1, col = "#696969") +
-  scale_fill_brewer(palette = "Set2") +
-  scale_y_continuous(limits = c(0, 150) , oob = scales::rescale_none) +
-  facet_wrap(~n) +
-  theme(
-    panel.border = element_rect(fill = "transparent", size = 0.4),
-    panel.spacing = unit(0, "line")
-  )
+theta_plot(
+  list("samples" = theta, "truth" = theta_truth, "fit" = theta_fit),
+  aligned = TRUE
+) %>%
+  ggsave(file = file.path(plot_dir, "theta_vb.pdf"))
 
 ## ---- vb-samples-beta ----
 beta_samples <- file.path(output_dir, "beta_samples_vb.feather") %>%
   read_feather()
 colnames(beta_samples) <- c("rep", "k", "v", "value")
-
 beta_samples$v <- factor(beta_samples$v, levels = v_order)
 
-ggplot() +
-  geom_vline(data = mbeta_truth, aes(xintercept = value, y = 0, col = as.factor(k)), size = 0.5, linetype = 1) +
-  geom_histogram(data = beta_samples,
-                 aes(x = value, fill = as.factor(k)),
-                 binwidth = .003, alpha = 0.8, position = "identity") +
-  geom_hline(yintercept = 0, size = 0.1, col = "#696969") +
-  scale_fill_brewer(palette = "Set2") +
-  scale_color_brewer(palette = "Set2") +
-  scale_y_continuous(expand = c(0, 0)) +
-  coord_flip() +
-  facet_grid(. ~ v) +
-  theme(
-    panel.spacing = unit(0, "line")
-  )
+beta_plot(
+  list("samples" = beta_samples, "truth" = mbeta_truth),
+  aligned = TRUE
+) %>%
+  ggsave(file = file.path(plot_dir, "beta_vb.pdf"))
+
 
 ## ---- gibbs-samples-beta ----
 beta_samples <- file.path(output_dir, "beta_samples_gibbs.feather") %>%
@@ -261,20 +217,11 @@ beta_samples <- match_matrices(
 colnames(beta_samples) <- c("rep", "k", "v", "value")
 beta_samples$v <- factor(beta_samples$v, levels = v_order)
 
-ggplot() +
-  geom_vline(data = mbeta_truth, aes(xintercept = value, y = 0, col = as.factor(k)), size = 0.5, linetype = 1) +
-  geom_histogram(data = beta_samples,
-                 aes(x = value, fill = as.factor(k)),
-                 binwidth = .003, alpha = 0.8, position = "identity") +
-  geom_hline(yintercept = 0, size = 0.1, col = "#696969") +
-  scale_fill_brewer(palette = "Set2") +
-  scale_color_brewer(palette = "Set2") +
-  scale_y_continuous(expand = c(0, 0)) +
-  coord_flip() +
-  facet_grid(. ~ v) +
-  theme(
-    panel.spacing = unit(0, "line")
-  )
+beta_plot(
+  list("samples" = beta_samples, "truth" = mbeta_truth),
+  aligned = TRUE
+) %>%
+  ggsave(file = file.path(plot_dir, "beta_gibbs.pdf"))
 
 ## ---- gibbs-samples-thtea ----
 theta_samples <- file.path(output_dir, "theta_samples_gibbs.feather") %>%
@@ -287,15 +234,10 @@ theta_samples <- match_matrices(
     select(-n) %>%
     as.matrix()
 )
+colnames(theta_samples) <- c("rep", "n", "k", "theta")
 
-ggplot() +
-  geom_histogram(data = theta_samples, aes(x = theta, fill = as.factor(k)),
-                 binwidth = 0.01, alpha = 0.8, position = "identity") +
-  geom_vline(data = theta_truth, aes(xintercept = value), linetype = 1, col = "#696969") +
-  scale_fill_brewer(palette = "Set2") +
-  scale_y_continuous(limits = c(0, 150) , oob = scales::rescale_none) +
-  facet_wrap(~n) +
-  theme(
-    panel.border = element_rect(fill = "transparent", size = 0.4),
-    panel.spacing = unit(0, "line")
-  )
+theta_plot(
+  list("samples" = theta_samples, "truth" = theta_truth),
+  aligned = TRUE
+) %>%
+  ggsave(file = file.path(plot_dir, "theta_gibbs.pdf"))
