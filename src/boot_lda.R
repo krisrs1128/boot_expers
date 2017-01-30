f8b1b2b0c9c152e3da2225169ed26c95d36d9e33
Kris Sankaran <sankaran.kris@gmail.com>
2017-01-30 13:26:32 -0800

    Debugging some of the bootstrapping functions

--- src/boot_lda.R
+++ src/boot_lda.R
@@ -23,6 +23,7 @@ library("data.table")
 source("../src/lda.R")
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
+dir.create(output_dir)
 
 ## ---- utils ----
 posterior_mean <- function(samples, dims) {
@@ -48,22 +49,24 @@ for (i in seq_len(n_replicates)) {
     stan_data <- list(
        "n" = cur_data,
        "K" = ncol(theta_hat),
-       "V" = ncol(beta_hat),
+       "V" = nrow(beta_hat),
        "D" = nrow(theta_hat),
        "alpha" = rep(alpha, ncol(theta_hat)),
-       "gamma" = rep(gamma, nrow(theta_hat))
+       "gamma" = rep(gamma, nrow(beta_hat))
     )
 
     vb_fit <- vb(
         stan_model("../src/lda.stan"),
         stan_data,
-        iter = n_iter
+        iter = n_samples
     ) %>%
         extract()
 
-    beta_hat <- posterior_mean(vb_fit$beta, c("v", "k"))
-    theta_hat <- posterior_mean(vb_fit$theta, c("i", "k"))
+    beta_boot <- posterior_mean(vb_fit$beta, c("v", "k")) %>%
+        melt(varnames = c("v", "k"), value.name = "beta")
+    theta_boot <- posterior_mean(vb_fit$theta, c("i", "k")) %>%
+        melt(varnames = c("i", "k"), value.name = "theta")
 
-    write_feather(beta_hat, paste0(output_dir, "beta-boot-", fit_id, batch_id, i, ".feather"))
-    write_feather(theta_hat, paste0(output_dir, "theta-boot-", fit_id, batch_id, i, ".feather"))
+    write_feather(beta_boot, file.path(output_dir, paste0("beta-boot-", fit_id, batch_id, i, ".feather")))
+    write_feather(theta_boot, file.path(output_dir, paste0("theta-boot-", fit_id, batch_id, i, ".feather")))
 }
