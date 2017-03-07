a88c51d07fd3e22b34e9b6186c8a893469f72d25
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-07 14:07:47 -0800

    Move reading files to package

---
+++ ldaSim/R/read.R
@@ -0,0 +1,44 @@
+#! /usr/bin/env Rscript
+
+## File description -------------------------------------------------------------
+## Read in the results from the LDA experiment
+##
+## author: kriss1@stanford.edu
+
+#' Bind together lists, not losing track of filenames
+#' @export
+cbind_list <- function(data_list, cbind_vals, cbind_name) {
+  ## bind together, not losing tracks of filename
+  for (i in seq_along(data_list)) {
+    if (nrow(data_list[[i]]) == 0) next
+    data_list[[i]] <- data.table(
+      cbind_vals[[i]],
+      data_list[[i]]
+    )
+    colnames(data_list[[i]])[1] <- cbind_name
+  }
+  data_list
+}
+
+#' Read RData from a vector of paths
+#' @export
+rdata_from_paths <- function(paths, param, var_names = NULL) {
+  data <- paths %>%
+    lapply(function(x) {
+      res <- rstan::extract(get(load(x)))
+      melt(res[[param]], varnames = var_names, value.name = "value")
+    }) %>%
+    cbind_list(paths, "file")
+  rbindlist(data)
+}
+
+#' Read feathers from a vector of paths
+#' @export
+feather_from_paths <- function(paths) {
+  ## read data into list
+  data <- paths %>%
+    lapply(read_feather) %>%
+    cbind_list(paths, "file")
+
+  rbindlist(data, fill = TRUE)
+}
--- vis/vis_utils.R
+++ vis/vis_utils.R
@@ -6,38 +6,6 @@
 ## author: kriss1@stanford.edu
 
 ## ---- data-reading ----
-cbind_list <- function(data_list, cbind_vals, cbind_name) {
-  ## bind together, not losing tracks of filename
-  for (i in seq_along(data_list)) {
-    if (nrow(data_list[[i]]) == 0) next
-    data_list[[i]] <- data.table(
-      cbind_vals[[i]],
-      data_list[[i]]
-    )
-    colnames(data_list[[i]])[1] <- cbind_name
-  }
-  data_list
-}
-
-rdata_from_paths <- function(paths, param, var_names = NULL) {
-  data <- paths %>%
-    lapply(function(x) {
-      res <- rstan::extract(get(load(x)))
-      melt(res[[param]], varnames = var_names, value.name = "value")
-    }) %>%
-    cbind_list(paths, "file")
-  rbindlist(data)
-}
-
-feather_from_paths <- function(paths) {
-  ## read data into list
-  data <- paths %>%
-    lapply(read_feather) %>%
-    cbind_list(paths, "file")
-
-  rbindlist(data, fill = TRUE)
-}
-
 
 ## ---- plots ----
 #' Errors histogram
