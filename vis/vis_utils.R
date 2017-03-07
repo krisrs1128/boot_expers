41c4c4f9ce799c284941f8c0f726ba1cba266cc0
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-07 14:23:08 -0800

    Remove vis utils, and no need for folder for vis

--- vis/vis_utils.R
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env Rscript
-
-## File description ------------------------------------------------------------
-## Utilities for visualizing output from lda simulation pipeline.
-##
-## author: kriss1@stanford.edu
-
-## ---- data-reading ----
-
