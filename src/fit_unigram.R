d65d249cdd40f89db43996de94991ab6f5e36e8b
Kris Sankaran <sankaran.kris@gmail.com>
2017-09-19 20:42:58 -0700

    skeleton for fitting unigram models

--- src/fit_unigram.R
+++ src/fit_unigram.R
@@ -16,11 +16,13 @@ library("rstan")
 
 args <- commandArgs(trailingOnly = TRUE)
 output_dir <- args[[1]]
-fit_id <- args[[2]]
-data_path <- args[[3]]
-n_samples <- as.integer(args[[4]])
-a0 <- as.numeric(args[[5]])
-b0 <- as.numeric(args[[6]])
+fit_method <- args[[2]]
+stan_path <- args[[3]]
+fit_id <- args[[4]]
+data_path <- args[[5]]
+n_samples <- as.integer(args[[6]])
+a0 <- as.numeric(args[[7]])
+b0 <- as.numeric(args[[8]])
 
 ###############################################################################
 ## prepare model input
@@ -40,4 +42,59 @@ stan_data <- list(
   "times" = seq_len(nrow(X)),
   "times_mapping" = seq_len(nrow(X)),
   "x" = X
+)
+
+###############################################################################
+## fit the model
+###############################################################################
+if (tolower(fit_method) == "vb") {
+  fit <- vb(
+    stan_model(stan_path),
+    data = stan_data,
+    output_samples = n_samples
+  )
+} else if (tolower(fit_method) == "gibbs") {
+  fit <- stan(
+    stan_path,
+    data = stan_data,
+    chains = 1,
+    iter = 2 * n_samples
+  )
+} else {
+  stop("fit_method must be either 'gibbs' or 'vb'")
+}
+
+###############################################################################
+## save results
+###############################################################################
+output_path <- file.path(
+  output_dir,
+  sprintf("%s-%s.RData", fit_method, gen_id)
+)
+save(fit, file = output_path)
+
+###############################################################################
+##  update the metdata
+###############################################################################
+metadata <- data.frame(
+  "file" = output_path,
+  "D" = nrow(X),
+  "V" = ncol(X),
+  "N" = sum(X[, 1]),
+  "sigma0" = NA,
+  "a0" = a0,
+  "b0" = b0,
+  "n_samples" = n_samples,
+  "method" = fit_method,
+  "iteration" = NA
+)
+
+metadata_path <- file.path(output_dir, "metadata.csv")
+write.table(
+  metadata,
+  file = metadata_path,
+  append = TRUE,
+  sep = ",",
+  row.names = FALSE,
+  col.names = !file.exists(metadata_path)
 )
