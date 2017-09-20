0ccf03aca8c11854cabac0206a9b21a3565cd895
Kris Sankaran <sankaran.kris@gmail.com>
2017-09-20 09:59:03 -0700

    move utils to separate file (allow reuse)

--- src/sim_unigram.R
+++ src/sim_unigram.R
@@ -14,6 +14,7 @@
 library("feather")
 library("tidyverse")
 library("reshape2")
+source("../src/unigram_utils.R") # assumed running from pipeline dir
 
 args <- commandArgs(trailingOnly = TRUE)
 argv <- list()
@@ -22,29 +23,13 @@ argv$gen_id <- args[[2]]
 argv$N <- as.numeric(args[[3]])
 argv$mu_path <- args[[4]]
 
-softmax <- function(x) {
-  exp(x) / log(sum(exp(x)))
-}
-
-#' Simulate from a unigram model
-unigram_data <- function(N, mu) {
-  mu_mat <- mu %>%
-    spread(v, mu) %>%
-    select(-i)
-  X <- matrix(nrow = nrow(mu_mat), ncol = ncol(mu_mat))
-
-  for (i in seq_len(nrow(mu_mat))) {
-    X[i, ] <- rmultinom(1, N, prob = softmax(mu_mat[i, ]))[, 1]
-  }
-
-  X
-}
-
 ###############################################################################
 ## Simulate and write results to file
 ###############################################################################
-mu <- read_feather(argv$mu_path)
-X <- unigram_data(argv$N, mu)
+mu_mat <- read_feather(argv$mu_path) %>%
+  spread(v, mu) %>%
+  select(-i)
+X <- unigram_data(argv$N, mu_mat)
 
 dir.create(argv$output_dir)
 x_path <- file.path(argv$output_dir, sprintf("x-%s.feather", argv$gen_id))
