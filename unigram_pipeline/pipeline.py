94c5d63a7fb2fbd38090e4070c3b128eb52ab340
Kris Sankaran <sankaran.kris@gmail.com>
2017-09-19 19:56:33 -0700

    unigram data generating class

--- unigram_pipeline/pipeline.py
+++ unigram_pipeline/pipeline.py
@@ -46,6 +46,44 @@ def run_and_check(cmds):
 ###############################################################################
 # Core pipeline classes
 ###############################################################################
+class UnigramData(luigi.Task):
+    """
+    Simulate data according to a Unigram model
+
+    Arguments:
+      D (int): How many samples are there in this experiment?
+      N (int): How many words are there in each sample?
+      V (int): How many terms are there across samples?
+      sigma0 (float): What is the true sigma random walk size parameter used in
+      generating the data?
+    """
+    D = luigi.Parameter()
+    N = luigi.Parameter()
+    V = luigi.Parameter()
+    sigma0 = luigi.Parameter()
+
+    conf = configuration.get_config()
+
+    def requires(self):
+        return UnigramParams(self.D, self.V, self.sigma0)
+
+    def run(self):
+        mu_path = self.input()[0].open("r").name
+        gen_id = hash_string("".join([self.D, self.N, self.V, self.sigma0]))
+        run_cmd = [
+            "Rscript",
+            self.conf.get("expers", "sim_script"),
+            self.conf.get("expers", "output_dir"),
+            gen_id,
+            self.N,
+            mu_path
+        ]
+        run_and_check(run_cmd)
+
+    def output(self):
+        gen_id = hash_string("".join([self.D, self.N, self.V, self.sigma0]))
+        output_dir = self.conf.get("expers", "output_dir")
+        return luigi.LocalTarget(os.path.join(output_dir, "x-" + gen_id + ".feather"))
 
 
 class UnigramParams(luigi.ExternalTask):
@@ -68,6 +106,7 @@ class UnigramParams(luigi.ExternalTask):
     conf = configuration.get_config()
 
     def run(self):
+        print("test")
         gen_id = hash_string("".join([self.D, self.V, self.sigma0]))
         print(gen_id)
         run_cmd = [
