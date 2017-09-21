5fd33036f392684cd7eaf12b71b65fd7f12f7f98
Kris Sankaran <sankaran.kris@gmail.com>
2017-09-21 08:53:40 -0700

    changing parameters to give more involved simulations

--- lda_pipeline/build_config.R
+++ lda_pipeline/build_config.R
@@ -1,4 +1,4 @@
 library("jsonlite")
-df = expand.grid(D = c(20, 100), V = c(10, 50), K = 2, N = c(20, 50, 100), alpha0 = 1, gamma0 = 1)
+df = expand.grid(D = c(20, 100), V = c(750, 1500), K = 2, N = c(3750, 7500, 15000), alpha0 = 1, gamma0 = 1)
 df$id <- 1:nrow(df)
 cat(toJSON(df, auto_unbox = TRUE), file = "experiment.json")
